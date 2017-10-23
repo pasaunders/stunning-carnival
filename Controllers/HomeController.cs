0b3f4c67e283aaf5823d151e2d777d02fb9ba5ca
pasaunders <kaorti@gmail.com>
2017-10-23 09:39:42 -0700

    asp.net intro

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace callingCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("{firstName}/{lastName}/{age}/{favoriteColor}")]
+        public JsonResult CallingCard(string firstName, string lastName, int age, string favoriteColor)
+        {
+            var cardData = new {
+                FirstName = firstName,
+                LastName = lastName,
+                Age = age,
+                color = favoriteColor
+            };
+            return Json(cardData);
+        }
+    }
+}
